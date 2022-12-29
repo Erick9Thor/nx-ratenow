4f2e39f4c62dc6363c05604a55a86c745d0a8cc3
Erick9Thor <erictoper@gmail.com>
2022-12-29 18:08:50 +0100

    Added first mfe

---
+++ apps/benchmark/module-federation.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'benchmark',
+  exposes: {
+    './Module': 'apps/benchmark/src/app/remote-entry/entry.module.ts',
+  },
+};
---
+++ apps/benchmark/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-ratenow-benchmark-entry',
+  template: `<nx-ratenow-nx-welcome></nx-ratenow-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
