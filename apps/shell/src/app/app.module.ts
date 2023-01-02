c983165427953aafac0381b23c0b5ccfecbb62d1
ofont <o.font@ratenow.es>
2023-01-02 17:08:00 +0100

    Revert "Revert "Changes to Table with filter, order and pepa.""
    This reverts commit c96b3d458349b003b878a95a54934a5600a977e2.

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { appRoutes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreMaterialModule } from '@nx-ratenow/core/material';
 import { CoreFeatureToolbarModule } from '@nx-ratenow/core/feature-toolbar';
+import {SharedFeatureTableModule} from "@nx-ratenow/shared/feature-table";
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +15,7 @@ import { CoreFeatureToolbarModule } from '@nx-ratenow/core/feature-toolbar';
     BrowserModule,
     CoreFeatureToolbarModule,
     CoreMaterialModule,
+    SharedFeatureTableModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     BrowserAnimationsModule,
   ],
--- libs/core/material/src/lib/core-material.module.ts
+++ libs/core/material/src/lib/core-material.module.ts
@@ -19,7 +19,6 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
@@ -27,24 +26,25 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatTreeModule } from '@angular/material/tree';
-import { OverlayModule } from '@angular/cdk/overlay';
-import { CdkMenuModule } from '@angular/cdk/menu';
-import { DialogModule } from '@angular/cdk/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatPaginatorModule } from "@angular/material/paginator";
+import {CdkMenuModule} from "@angular/cdk/menu";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTreeModule} from "@angular/material/tree";
+import {OverlayModule} from "@angular/cdk/overlay";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {DialogModule} from "@angular/cdk/dialog";
 
 @NgModule({
   exports: [
@@ -95,6 +95,7 @@ import { DialogModule } from '@angular/cdk/dialog';
     PortalModule,
     ScrollingModule,
     DialogModule,
+    MatPaginatorModule,
   ],
 })
 export class CoreMaterialModule {}
