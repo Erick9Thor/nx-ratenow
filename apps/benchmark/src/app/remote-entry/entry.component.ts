e060b9a4766b7c884e916afb177f53bb4ddcf2c4
Eric <e.torres@ratenow.es>
2023-01-11 14:14:55 +0100

    [DELETE] Unused entry component

--- apps/benchmark/src/app/remote-entry/entry.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'nx-ratenow-remote-benchmark',
-  template: ``
-})
-export class RemoteEntryComponent {}
--- apps/benchmark/src/app/remote-entry/entry.module.ts
+++ apps/benchmark/src/app/remote-entry/entry.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RemoteEntryComponent } from './entry.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
 import { BenchmarkDashboardFeatureModule } from '@nx-ratenow/benchmark/dashboard/feature';
 
 @NgModule({
   imports: [CommonModule, BenchmarkDashboardFeatureModule, TranslateModule],
-  declarations: [RemoteEntryComponent],
   exports: [TranslateModule, RouterModule]
 })
 export class RemoteEntryModule {}
